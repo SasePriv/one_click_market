a8705df08742f042a1c3d03cb355457bb0f22122
Luis S <luisalejandross@dynameat.io>
2024-02-08 13:22:46 +0100

    adding the frist view of frontend

--- frontend/src/components/ThemeSwitcher.tsx
+++ frontend/src/components/ThemeSwitcher.tsx
@@ -1,24 +1,29 @@
 "use client";
 
-import {useTheme} from "next-themes";
+import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import { Button } from "@nextui-org/button"
+import { Switch } from "@nextui-org/react";
+import { MoonIcon } from "./MoonIcon";
+import { SunIcon } from "./SunIcon";
 
 export function ThemeSwitcher() {
-  const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
 
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setMounted(true);
+  }, []);
 
-  if(!mounted) return null
+  if (!mounted) return null;
 
   return (
-    <div>
-      The current theme is: {theme}
-      <Button onClick={() => setTheme('light')}>Light Mode</Button>
-      <Button onClick={() => setTheme('dark')}>Dark Mode</Button>
-    </div>
-  )
-};
+    <Switch
+      defaultSelected
+      size='lg'
+      color='success'
+      startContent={<SunIcon />}
+      endContent={<MoonIcon />}>
+      Dark mode
+    </Switch>
+  );
+}
